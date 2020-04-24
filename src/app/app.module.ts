30ffe93f9831c9a8aa4249228743b73d1dd59a87
Bruno Moura <brunobsm88@gmail.com>
2020-04-23 21:43:16 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
+import { ResumoComponent } from './resumo/resumo.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PainelSimplesComponent,
+    BarraNavegacaoComponent,
+    MenuLateralComponent,
+    ResumoComponent,
+    ConsultaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
